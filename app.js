92c45e163e35004c8c3496276cdd91645ae0702b
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-07-30 09:55:29 +0200

    set up cors policy

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const userRouter = require('./api/users/user.router');
 const bookRouter = require('./api/books/book.router');
 
-app.use(express.json())
-.use('/api/users', userRouter)
-.use('/api/books', bookRouter)
+app.use(cors({
+  credentials: true,
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+  origin: ['*', 'http://localhost:3000/']
+}))
+
+  .use(express.json())
+  .use('/api/users', userRouter)
+  .use('/api/books', bookRouter)
 
 
-.listen(process.env.APP_PORT ,()=>{
-  console.log(`server started on port ${process.env.APP_PORT}`);
-})
+  .listen(process.env.APP_PORT, () => {
+    console.log(`server started on port ${process.env.APP_PORT}`);
+  })
